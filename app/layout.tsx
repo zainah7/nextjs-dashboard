f36ca2d10483a79e2e325d501039ba925d5df721
zainah7 <zainaabd7@gmail.com>
2024-10-27 17:29:33 +0100

    fix vercel error

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
     </html>
   );
 }
