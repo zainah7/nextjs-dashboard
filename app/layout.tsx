0cda198b4026405bee4c68ad53050ba76d4065c4
zainah7 <zainaabd7@gmail.com>
2024-10-27 17:55:56 +0100

    fix vercel error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
-import { ReactNode } from "react";
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
